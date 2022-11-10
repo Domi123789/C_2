163835d3e1dca07557c7d74b709fa607d4ed4bdd
Domi123789 <89708991+Domi123789@users.noreply.github.com>
2022-11-10 22:56:00 +0100

    Add files via upload

---
+++ 2dPolja_7.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr [2][2]= {{5,5}, {9,9}};
+    int i,j;
+
+    printf ("Arr: \n");
+    for (i=0; i<=1; i++)
+    {
+        for (j=0; j<=1; j++)
+        {
+            printf ("%d", arr[i][j]);
+        }
+        printf ("\n");
+    }
+
+    printf ("Transpose of Array is given below: \n");
+    for (i=0; i<=1; i++)
+    {
+        for (j=0; j<=1; j++)
+        {
+            printf ("%d", arr[j][i]);
+        }
+        printf ("\n");
+    }
+}
